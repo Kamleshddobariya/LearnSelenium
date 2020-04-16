d837c3d9a54aa9153d565dac7c7ed8331657933e
User <User@WFH_Kamlesh>
2020-04-16 18:59:16 +0530

    cssSelector

---
+++ TestNGConcepts/src/test/java/MyTests/cssSelector.java
@@ -0,0 +1,24 @@
+package MyTests;
+
+public class cssSelector {
+
+	public static void main(String[] args) {
+
+		// cssSelector rules
+		// 1. id => htmltag#id , #id
+		// 2. class => htmltag.classname, .classname, .c1.c2.c3
+		// 3. parent>childtag
+		// 4. htmltag[id='value']
+		// 4.a. input[id='username'][type='email'] --css with two properties
+		// 5. contains in css:
+		// input[id*='user']
+		// input[id*='name']
+		// input[id*test_]
+		// 6. start the text in css:
+		//input[id^='user']
+		// 7. ending the text in css:
+		//input[id^='name']			
+
+	}
+
+}
