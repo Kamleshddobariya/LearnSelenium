c36134ded7747429875f402fc2a7f2d929d2fccd
User <User@WFH_Kamlesh>
2020-04-16 15:59:48 +0530

    Test

--- CollectionsSessions/src/FinalConcepts/FinalConcepts.java
+++ CollectionsSessions/src/FinalConcepts/FinalConcepts.java
@@ -5,7 +5,7 @@ public class FinalConcepts {
 	public static void main(String[] args) {
 		
 		// final(keyword) concept is used to define constant value
-		// prevent inheritance to github
+		// prevent inheritance to github test
 		
 		final int i = 10;
 //		i = 20;
